dc42b183fbec3f252a4659bfc906e64f8b70c2c0
Ariel Tubaltsev <tubaltzev@gmail.com>
2016-03-31 13:24:00 -0700

    Jesse and Cookies

---
+++ JesseAndCookies.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Integer first, second;
+        Scanner sc=new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int count = 0;
+        
+        PriorityQueue<Integer> cookies = new PriorityQueue<Integer>();
+        for (int i=0; i<n; i++) {
+            cookies.add(sc.nextInt());
+        }
+        
+        while(true) {
+            if (cookies.peek() >= k) {
+                System.out.println(count);
+                return;
+            }
+            
+            if (cookies.size() > 1) { 
+              cookies.add(cookies.remove() + 2*cookies.remove());
+              count++;
+            } else {
+                break;
+            }
+        }
+        System.out.println("-1");
+    }
+}
