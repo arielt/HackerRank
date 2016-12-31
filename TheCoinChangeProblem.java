0adf03a5190d16d5cca664f56d3e18fe334e34c8
Ariel Tubaltsev <tubaltzev@gmail.com>
2016-12-30 17:54:59 -0800

    The Coin Change Problem

---
+++ TheCoinChangeProblem.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    private static long coins(int N, int M, int[] C) {
+        long t[][] = new long[M+1][N+1];
+
+        for (int j=0; j < N+1; j++) {
+            for (int i=0; i < M+1; i++) {
+                if(i==0 || j==0) {
+                    t[i][j] = 0;
+                } else {
+                    int rem = j - C[i-1];
+                    // +1 if remnant is 0
+                    if (rem == 0)
+                        t[i][j] = 1;
+                    // upper element
+                    t[i][j] += t[i-1][j];
+                    // left element
+                    if (rem > 0)
+                        t[i][j] += t[i][rem];
+                }
+            }
+        }
+
+        return t[M][N];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int arr[] = new int[M];
+        for(int i=0; i < M; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(coins(N, M, arr));
+    }
+}
