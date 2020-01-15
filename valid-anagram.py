0a9f48efee760d8b188ab7417884468bea528e8c
Ariel Tubaltsev <tubaltzev@gmail.com>
2020-01-14 23:23:42 -0800

    Valid Anagram

---
+++ valid-anagram.py
@@ -0,0 +1,11 @@
+# Given two strings s and t , write a function to determine if t is an anagram of s.
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return collections.Counter(s) == collections.Counter(t)
+        
