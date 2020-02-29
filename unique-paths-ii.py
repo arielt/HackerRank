b32acf0324403166bfe6ebd13d56678c342643eb
Ariel Tubaltsev <tubaltzev@gmail.com>
2020-02-29 09:10:20 -0800

    Unique Paths II

---
+++ unique-paths-ii.py
@@ -0,0 +1,41 @@
+# A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+# The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+# Now consider if some obstacles are added to the grids. How many unique paths would there be?
+
+class Solution(object):
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        """
+        :type obstacleGrid: List[List[int]]
+        :rtype: int
+        """
+        if obstacleGrid[0][0]:
+            return 0
+        
+        obstacleGrid[0][0] = 1
+        
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+        
+        for i in range (1,m):
+            if obstacleGrid[i][0] == 1:
+                obstacleGrid[i][0] = 0
+            else:
+                obstacleGrid[i][0] = obstacleGrid[i-1][0]
+                
+        for j in range (1,n):
+            if obstacleGrid[0][j] == 1:
+                obstacleGrid[0][j] = 0
+            else:
+                obstacleGrid[0][j] = obstacleGrid[0][j-1]
+        
+        for i in range (1,m):
+            for j in range (1,n):
+                if obstacleGrid[i][j] == 1:
+                    obstacleGrid[i][j] = 0
+                else:
+                    obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1]
+        
+        return obstacleGrid[m-1][n-1]
+
