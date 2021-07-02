344b23a68458039bf492bbc96a297a27ff1cfd82
Ariel Tubaltsev <tubaltzev@gmail.com>
2021-07-01 18:31:47 -0700

    Maximum Product Difference Between Two Pairs

---
+++ maximum-product-difference-between-two-pairs.py
@@ -0,0 +1,24 @@
+# Maximum Product Difference Between Two Pairs
+# The product difference between two pairs (a, b) and (c, d) is defined as (a * b) - (c * d).
+
+# For example, the product difference between (5, 6) and (2, 7) is (5 * 6) - (2 * 7) = 16.
+# Given an integer array nums, choose four distinct indices w, x, y, and z such that the product difference 
+# between pairs (nums[w], nums[x]) and (nums[y], nums[z]) is maximized.
+
+# Return the maximum such product difference.
+
+
+# Test results
+
+# Runtime: 152 ms, faster than 47.54% of Python online submissions for Maximum Product Difference Between Two Pairs.
+# Memory Usage: 14.7 MB, less than 15.14% of Python online submissions for Maximum Product Difference Between Two Pairs.
+
+class Solution(object):
+    def maxProductDifference(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        return nums[-1] * nums[-2] - nums[0] * nums[1]
+
